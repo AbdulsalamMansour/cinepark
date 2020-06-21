bd7eac6df23393eb9b1e7dc49d21fe173d3af608
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-06-21 12:33:34 -0700

    add project

---
+++ cinepark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cinepark
+//
+//  Created by Abdulsalam Mansour on 6/21/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
