ee53c1fca0d8939e9326ed5cb7fc6945e97bc358
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-06-21 13:53:39 -0700

    create TabBar and main screens

---
+++ cinepark/CustomViews/TabBarControllers/CPTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  CPTabBarController.swift
+//  cinepark
+//
+//  Created by Abdulsalam Mansour on 6/21/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import UIKit
+
+class CPTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = AppColors.primaryViolet
+        viewControllers                 = [createMostViewedVC(), createFavoritesNC()]
+    }
+    
+    
+    func createMostViewedVC() -> UINavigationController {
+        let mostViewedVC        = MostViewedVC()
+        mostViewedVC.title      = "Most Viewed"
+        mostViewedVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+        
+        return UINavigationController(rootViewController: mostViewedVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC         = FavoritesListVC()
+        favoritesListVC.title       = "Favorites"
+        favoritesListVC.tabBarItem  = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
---
+++ cinepark/Utilities/AppColors.swift
@@ -0,0 +1,13 @@
+//
+//  AppColors.swift
+//  cinepark
+//
+//  Created by Abdulsalam Mansour on 6/21/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import UIKit
+struct AppColors {
+    static var primaryViolet: UIColor {return UIColor(red: 0.72, green: 0.04, blue: 0.84, alpha: 1.00)}
+
+}
