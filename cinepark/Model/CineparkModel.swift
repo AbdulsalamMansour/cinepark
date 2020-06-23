99012a03526284f905c3c3c8764cfade5785fe37
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-06-23 02:58:50 -0700

    add cinepark model

---
+++ cinepark/Model/CineparkModel.swift
@@ -0,0 +1,33 @@
+//
+//  CineparkModel.swift
+//  cinepark
+//
+//  Created by Abdulsalam Mansour on 6/23/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import Foundation
+
+struct CineparkItem: Codable, Hashable{
+    
+    var posterPath: String?
+    var id: Int?
+    var originalLanguage: String?
+    var title: String?
+    var voteAverage: Double?
+    var overview, releaseDate: String?
+    var name: String?
+    var firstAirDate: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath         = "poster_path"
+        case id
+        case originalLanguage   = "original_language"
+        case title
+        case voteAverage        = "vote_average"
+        case overview
+        case releaseDate        = "release_date"
+        case name
+        case firstAirDate       = "first_air_date"
+    }
+}
